8118afa59666eec6d2c2a0b1f4a9e1ecca04af89
Roman Sommer <roman@resonant-bytes.de>
2021-03-03 11:56:28 +0100

    Fix LED and disable serial for now

--- demos/STM32/RT-STM32F407-OLIMEX_STM32_E407/main.c
+++ demos/STM32/RT-STM32F407-OLIMEX_STM32_E407/main.c
@@ -29,9 +29,9 @@ static THD_FUNCTION(Thread1, arg) {
   (void)arg;
   chRegSetThreadName("blinker");
   while (true) {
-    palSetPad(GPIOD, GPIOD_LED3);       /* Orange.  */
+    palSetPad(GPIOC, GPIOC_LED);       /* LED */
     chThdSleepMilliseconds(500);
-    palClearPad(GPIOD, GPIOD_LED3);     /* Orange.  */
+    palClearPad(GPIOC, GPIOC_LED);     /* LED */
     chThdSleepMilliseconds(500);
   }
 }
@@ -55,9 +55,9 @@ int main(void) {
    * Activates the serial driver 2 using the driver default configuration.
    * PA2(TX) and PA3(RX) are routed to USART2.
    */
-  sdStart(&SD2, NULL);
-  palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7));
-  palSetPadMode(GPIOA, 3, PAL_MODE_ALTERNATE(7));
+  /* sdStart(&SD2, NULL); */
+  /* palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7)); */
+  /* palSetPadMode(GPIOA, 3, PAL_MODE_ALTERNATE(7)); */
 
   /*
    * Creates the example thread.
@@ -69,10 +69,11 @@ int main(void) {
    * sleeping in a loop and check the button state.
    */
   while (true) {
-    if (palReadPad(GPIOA, GPIOA_BUTTON)) {
-      test_execute((BaseSequentialStream *)&SD2, &rt_test_suite);
-      test_execute((BaseSequentialStream *)&SD2, &oslib_test_suite);
+    if (palReadPad(GPIOA, GPIOA_BUTTON_WKUP)) {
+      /* test_execute((BaseSequentialStream *)&SD2, &rt_test_suite); */
+      /* test_execute((BaseSequentialStream *)&SD2, &oslib_test_suite); */
+	palTogglePad(GPIOC, GPIOC_LED);       /* LED */
     }
-    chThdSleepMilliseconds(500);
+    chThdSleepMilliseconds(50);
   }
 }
